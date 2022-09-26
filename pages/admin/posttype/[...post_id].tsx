364482fade263f326b1aa2cf123d00d8c6e8decb
runemark7 <alexanderrunemark@gmail.com>
2022-09-26 15:31:15 +0200

    + make it possible to change postdata/postmeta + index file for admin create posttype

--- pages/admin/posttype/[...post_id].tsx
+++ pages/admin/posttype/[...post_id].tsx
@@ -1,33 +1,84 @@
 import type {GetStaticPaths, GetStaticProps, NextPage} from 'next'
 import PostMeta from "../../../models/PostMeta";
-import {FormEvent} from "react";
+import {FormEvent, useState} from "react";
+import {useSelector} from "react-redux";
 
 const AdminUpdateSinglePost: NextPage = (data: any) => {
-    if (!data.postData) return <p>No profile data</p>
+    if (!data) return <p>No info found</p>
+
+    const [changedPostData, setChangedPostData] = useState(null);
+    const [changedPostMeta, setChangedPostMeta] = useState(null);
+
+    const [isLoading, setLoading] = useState(false);
+
+    const user = useSelector((state)=>(state.user))
+
+    const handleSubmit = (e: FormEvent, postId) =>{
+        e.preventDefault();
+
+        const payload = {
+            "postId": postId,
+            "postData": changedPostData,
+            "postMeta": changedPostMeta
+        }
+
+        const JSONdata = JSON.stringify(payload);
+
+        const endpoint = "http://localhost:8010/proxy/api/update/post"
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + user.jwtToken,
+            },
+            body:JSONdata
+        }
+
+        fetch(endpoint, options)
+            .then(resp=>resp.json())
+            .then(data => {
+                setLoading(false)
+            })
+
+    }
+
+    const handleMetaDataChanges = (e: FormEvent) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        // @ts-ignore
+        setChangedPostMeta({...changedPostMeta, [name]: value})
+    }
+
+    const handlePostDataChanges = (e: FormEvent) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        // @ts-ignore
+        setChangedPostData({...changedPostData, [name]: value})
+    }
 
     return (
         <div>
-            <form className={"formHolder"} method="post" onSubmit={(e)=>{
-                updatePost(e)
+            <form className={"formHolder"} onSubmit={(e:FormEvent)=>{
+                handleSubmit(e, data.postData.post_id);
             }} >
                 <div className={"productWrapper"}>
                     <div>
                         <p> id: {data.postData.post_id} </p>
                         <label htmlFor="post_name">PostName</label>
-                        <input type={"text"} name={"post_name"} value={data.postData.post_name}/>
+                        <input type={"text"} name={"post_name"} defaultValue={data.postData.post_name} onChange={handlePostDataChanges} />
 
                         <label htmlFor="post_status">Post status</label>
-                        <input type={"text"} name={"post_status"} value={data.postData.post_status}/>
+                        <input type={"text"} name={"post_status"} defaultValue={data.postData.post_status} onChange={handlePostDataChanges} />
                     </div>
                     <br/>
                     <div>
                         <h4>meta values</h4>
-                        {data.postMeta.map((postMeta: PostMeta) => (
+                        {(data.postMeta.length != 0)?data.postMeta.map((postMeta: PostMeta) => (
                             <div className={"productWrapper"} key={postMeta.meta_id}>
                                 <p className={"productTitle"}>meta: {postMeta.meta_key}</p>
-                                <input type="text" name={postMeta.meta_key} value={postMeta.meta_value} />
+                                <input type={"text"} name={postMeta.meta_key} defaultValue={postMeta.meta_value} onChange={handleMetaDataChanges} />
                             </div>
-                        ))}
+                        )):<div>Can't find any meta values</div>}
                     </div>
 
                 </div>
@@ -38,56 +89,10 @@ const AdminUpdateSinglePost: NextPage = (data: any) => {
     )
 }
 
-const updatePost = async (e: FormEvent) => {
-    e.preventDefault()
-
-    const postData = {
-        post_name: e.target.post_name.value,
-        post_type: e.target.post_status.value
-    }
-
-    //TODO: don't know how to get all the meta here :C
-    const postMeta = [
-        {
-
-        }
-    ]
-
-    const returnObj = {
-        "postData": postData,
-        "postMeta": postMeta
-    }
-
-    const JSONdata = JSON.stringify(returnObj);
-
-    const endpoint = "http://localhost:8010/proxy/api/posttypes"
-
-    const options = {
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Headers': "*",
-            "Access-Control-Allow-Origin": "*"
-        },
-        body: JSONdata
-    }
-
-    // @ts-ignore
-    const response = await fetch(endpoint, options)
-        .then(resp=>resp.json())
-        .then(data => {
-                console.log("success");
-                console.log(data);
-            }
-        );
-
-}
-
-
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
     // @ts-ignore
-    const post_id = params.post_id[1]
+    const post_id = params.post_id[2]
 
     const data = await fetch(`http://localhost:8010/proxy/api/post/${post_id}`);
     const postData = await data.json();
@@ -96,13 +101,11 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
     }
 }
 
-//TODO: should check if second param is numeric and there is not something else there too.
 export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
     return {
         paths: [], //indicates that no page needs be created at build time
         fallback: 'blocking' //indicates the type of fallback
     }
 }
-
 
 export default AdminUpdateSinglePost
